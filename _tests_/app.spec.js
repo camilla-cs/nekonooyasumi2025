42d6c9579de5af62a632aaffabfc66079e98d8ab
Camilla De Pretto <161570720+camilla-cs@users.noreply.github.com>
2025-04-21 14:18:55 +1000

    test: tested game lost feature

--- _tests_/app.spec.js
+++ _tests_/app.spec.js
@@ -25,3 +25,34 @@ test('START link goes to game page', async() => {
     
     await electronApp.close(); 
 }); 
+
+test('when lives go down to 0, game lost page appears', async() => {
+    const electronApp = await electron.launch({args:['main.js']}); 
+
+    const mainWindow = await electronApp.firstWindow(); 
+
+    const gameLink = await mainWindow.waitForSelector('a[href*="game.html"]'); 
+
+    await gameLink.click(); 
+    await mainWindow.waitForLoadState('domcontentloaded'); 
+
+
+    // lives = 0 
+    for (let i= 0; i<9; i++) {
+        const wrongButton = await mainWindow.waitForSelector('button:text("z")'); 
+        await wrongButton.click();  
+        await mainWindow.waitForTimeout(300); 
+    }
+
+
+    // user redirected to game lost page 
+    await mainWindow.waitForURL(/gameLost\.html/); 
+    const gameLostUrl = await mainWindow.url(); 
+    expect(gameLostUrl).toContain('gameLost.html'); 
+
+    // app quits 
+    await electronApp.close(); 
+
+
+}); 
+
